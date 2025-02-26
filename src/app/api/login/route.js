6287987cf36d6f50c35690335990e2b5e97683c2
Sahil verma <129593012+sahil-verma-9696@users.noreply.github.com>
2025-02-26 15:48:22 +0000

    deleting  app/ duplicates

--- src/app/api/login/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-// Handle GET requests to /api/login
-export async function GET(request) {
-  return NextResponse.json({ message: 'GET request received' });
-}
